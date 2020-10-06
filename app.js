7c3f240585fc05b59032581264e8b6412e5d1ec5
Ethan Hansen <hanseneg@hotmail.com>
2020-10-06 14:24:38 -0600

    commit2

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
         const JSONdata = xhr.responseText
         const data = JSON.parse(JSONdata)
+        showData(data.objects[0].pokemon)
+    }
+}
+
+function showData(arr){
+    for(let i = 0; i < arr.length; i++){
+        const h1 = document.createElement('h1')
+        h1.textContent = arr[i].name
+        document.body.appendChild(h1)
     }
 }
